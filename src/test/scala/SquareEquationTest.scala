a454d5037cba1072a678e32be6f99be2284a6ba7
YusupovPavel <yusalar@mail.ru>
2021-12-19 20:29:11 +0300

    tests reorganized, one root test reworked

--- src/test/scala/SquareEquationTest.scala
+++ src/test/scala/SquareEquationTest.scala
@@ -15,63 +15,72 @@ class SquareEquationTest extends AnyWordSpecLike with Matchers {
     }
 
     "have one root" in {
-      val roots: List[Double] = SquareEquation.solve(a = 1.0, b = -2.0, c = 1.0)
+      val roots: List[Double] = SquareEquation.solve(a = 1.0, b = -2.0, c = 1.000000000000001)
       roots should be (1.0 :: 1.0 :: Nil)
-    }
-
-    "throw exception when a = 0" in {
-      assertThrows[IllegalArgumentException] {
-        SquareEquation.solve(a = 0.0, b = 1.0, c = 1.0)
-      }
-    }
-
-    "throw exception when a = Inf" in {
-      assertThrows[IllegalArgumentException] {
-        SquareEquation.solve(a = Double.PositiveInfinity, b = 1.0, c = 1.0)
-      }
-
-      assertThrows[IllegalArgumentException] {
-        SquareEquation.solve(a = Double.NegativeInfinity, b = 1.0, c = 1.0)
-      }
-    }
-
-    "throw exception when b = Inf" in {
-      assertThrows[IllegalArgumentException] {
-        SquareEquation.solve(a = 1.0, b = Double.PositiveInfinity, c = 1.0)
-      }
-
-      assertThrows[IllegalArgumentException] {
-        SquareEquation.solve(a = 1.0, b = Double.NegativeInfinity, c = 1.0)
-      }
-    }
-
-    "throw exception when c = Inf" in {
-      assertThrows[IllegalArgumentException] {
-        SquareEquation.solve(a = 1.0, b = 1.0, c = Double.PositiveInfinity)
-      }
-
-      assertThrows[IllegalArgumentException] {
-        SquareEquation.solve(a = 1.0, b = 1.0, c = Double.NegativeInfinity)
-      }
-    }
-
-    "throw exception when a = NaN" in {
-      assertThrows[IllegalArgumentException] {
-        SquareEquation.solve(a = Double.NaN, b = 1.0, c = 1.0)
-      }
-    }
-
-    "throw exception when b = NaN" in {
-      assertThrows[IllegalArgumentException] {
-        SquareEquation.solve(a = 1.0, b = Double.NaN, c = 1.0)
-      }
-    }
-
-    "throw exception when c = NaN" in {
-      assertThrows[IllegalArgumentException] {
-        SquareEquation.solve(a = 1.0, b = 1.0, c = Double.NaN)
-      }
     }
   }
 
+  "square equation" should {
+    "throw exception" when {
+      "a = 0" in {
+        assertThrows[IllegalArgumentException] {
+          SquareEquation.solve(a = 0.0, b = 1.0, c = 1.0)
+        }
+      }
+
+      "a = +Inf" in {
+        assertThrows[IllegalArgumentException] {
+          SquareEquation.solve(a = Double.PositiveInfinity, b = 1.0, c = 1.0)
+        }
+      }
+
+      "a = -Inf" in {
+        assertThrows[IllegalArgumentException] {
+          SquareEquation.solve(a = Double.NegativeInfinity, b = 1.0, c = 1.0)
+        }
+      }
+
+      "a = NaN" in {
+        assertThrows[IllegalArgumentException] {
+          SquareEquation.solve(a = Double.NaN, b = 1.0, c = 1.0)
+        }
+      }
+
+      "b = +Inf" in {
+        assertThrows[IllegalArgumentException] {
+          SquareEquation.solve(a = 1.0, b = Double.PositiveInfinity, c = 1.0)
+        }
+      }
+
+      "b = -Inf" in {
+        assertThrows[IllegalArgumentException] {
+          SquareEquation.solve(a = 1.0, b = Double.NegativeInfinity, c = 1.0)
+        }
+      }
+
+      "b = NaN" in {
+        assertThrows[IllegalArgumentException] {
+          SquareEquation.solve(a = 1.0, b = Double.NaN, c = 1.0)
+        }
+      }
+
+      "c = +Inf" in {
+        assertThrows[IllegalArgumentException] {
+          SquareEquation.solve(a = 1.0, b = 1.0, c = Double.PositiveInfinity)
+        }
+      }
+
+      "c = -Inf" in {
+        assertThrows[IllegalArgumentException] {
+          SquareEquation.solve(a = 1.0, b = 1.0, c = Double.NegativeInfinity)
+        }
+      }
+
+      "c = NaN" in {
+        assertThrows[IllegalArgumentException] {
+          SquareEquation.solve(a = 1.0, b = 1.0, c = Double.NaN)
+        }
+      }
+    }
+  }
 }
